78e26632b0f6181b4dea2623ba8a2645d9a6b8f6
cavaglia <cavagliam@gmail.com>
2020-12-02 13:01:05 -0600

    Update real_data.py

--- real_data.py
+++ real_data.py
@@ -1,4 +1,8 @@
-#Downloads and prepares the data 
+#Downloads and prepares the data.
+#Usage: Put the GWOSC data file [data_file] in a Data directory. This reads [data_length] seconds of data in the file starting from start_time (in seconds).
+#For example: real_data(4096,10,1,H-H1_GWOSC_O2_4KHZ_R1-1181642752-4096.hdf5) reads 1 second of data starting from second 10 in the file
+#H-H1_GWOSC_O2_4KHZ_R1-1181642752-4096.hdf5. This function can be used to loop over the file and separate the data in chunks of data_length seconds.
+#It returns a pandas dataframe with the time stamps and the strain values.
 def real_data(sampling_rate,start_time,data_length,data_file):
     end_time = start_time + data_length
     data_start = sampling_rate * start_time
