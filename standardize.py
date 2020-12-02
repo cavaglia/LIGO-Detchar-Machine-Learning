3c37c7818f8a561ac44fe9f545d374349feb4db2
cavaglia <cavagliam@gmail.com>
2020-12-02 13:03:18 -0600

    Update standardize.py

--- standardize.py
+++ standardize.py
@@ -1,4 +1,5 @@
-#Standardize the data so that the mean is zero and the standard deviation is 1
+#This function standardizes the data so that the mean is zero and the standard deviation is 1. Requires the dataframe made with real_data and returns the
+#standrdized strain
 def standardize(data):
     data_reshaped = np.asarray(data['value']).reshape((len(data['time']), 1))
     scaler = StandardScaler().fit(data_reshaped)
